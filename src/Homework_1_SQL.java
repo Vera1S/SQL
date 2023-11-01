51d377be4f18449c20f3ba47cf19a0b146731128
Vera <inginer719@gmail.ru>
2023-11-01 12:43:01 -0400

    Initial commit

---
+++ src/Homework_1_SQL.java
@@ -0,0 +1,39 @@
+public class Homework_1_SQL {
+    //1. Приведите десять типовых бизнес-процессов для предметной области ВИДЕО-ХОСТИНГ (Youtube),
+    // а также их соответствие по CRUD.
+    //В рамках БД "песочница" напишите след/запросы:
+
+    //Задача 1. Вывести название и стоимость товаров от 20 EUR.
+         // SELECT ProductName, Price
+         //FROM [Products]
+          //WHERE  Price >= 20
+
+    //Задача 2. Вывести страны поставщиков.
+//    SELECT Country
+//    FROM [Suppliers]
+
+    //Задача 3. В упорядоченном по стоимости виде вывести название и стоимость товаров от всех поставщиков,
+    // кроме поставщика 1.
+    // SELECT ProductName, Price, SupplierID
+    //FROM [Products]
+    //WHERE NOT SupplierID = '1'
+
+    //Задача 4. Вывести контактные имена клиентов, кроме тех, что из France и USA.
+    // SELECT ContactName, Country
+    //FROM [Customers]
+    //WHERE NOT Country = 'France'
+    //AND
+    //NOT Country = 'USA'
+
+    //Задача 5. Вывести два самых дорогих товара из категории 4
+    //SELECT *
+    //FROM [Products]
+    //WHERE CategoryID = 5
+    //ORDER BY Price DESC
+    //LIMIT 2
+    //
+    //Конспект занятия
+    //https://western-appeal-39b.notion.site/GenTech-1-Oct-26-2023-12b3a0d0858147ea8d76359bc9eae101?pvs=4
+
+
+}
